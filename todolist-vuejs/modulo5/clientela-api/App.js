2dfa0cb2493cb52808a1d67e1b1d2e6520e1b489
Paulo Arruda <paulo.arruda85@gmail.com>
2019-11-26 20:24:55 -0300

    conexao com postgres

--- todolist-vuejs/modulo5/clientela-api/App.js
+++ todolist-vuejs/modulo5/clientela-api/App.js
@@ -1,9 +1,36 @@
 const Express = require('express')
 const BodyParser = require('body-parser')
+const Pg = require('pg')
+const url = "postgressql://postgres:postgres@localhost:5432/jh"
+
+
+const client = new Pg.Client(url)
+
 
 const app = new Express()
 
-app.user(BodyParser.json())
+app.use(BodyParser.json())
+
+client.connect()
+
+// client.query('select * from bairro', (err, res) => {
+//     console.log(err, res)
+//     client.end()
+// })
+
+const query = 'select * from bairro where id = 145'
+
+client.query(query).then(res => {
+    const rows = res.rows
+
+    rows.map(row => {
+        console.log(`read : ${JSON.stringify(row)}`)
+    })
+
+    process.exit()
+}).catch(err => {
+    console.log(err)
+})
 
 
 app.get('/', (req, res) => {
